0c99de10b18a675697e16469d816d751ed821f25
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2015-11-21 17:01:57 +0100

    [BUGFIX] Add 'foreign_sortby' for relations to records with sorting
    If 'foreign_sortby' is set related records are sorted correctly
    upon fetching.
    
    Change-Id: I63815e1f99c38561889de95f4d707d174302c7c9
    Resolves: #68694
    Releases: 6.2
    Reviewed-on: https://review.typo3.org/44867
    Reviewed-by: Nico de Haen <mail@ndh-websolutions.de>
    Tested-by: Nico de Haen <mail@ndh-websolutions.de>

--- Tests/Fixtures/TestExtensions/test_extension_v3/ext_tables.sql
+++ Tests/Fixtures/TestExtensions/test_extension_v3/ext_tables.sql
@@ -71,6 +71,7 @@ CREATE TABLE tx_testextension_domain_model_child1 (
 	t3ver_count int(11) DEFAULT '0' NOT NULL,
 	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
 	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+	sorting int(11) DEFAULT '0' NOT NULL,
 
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
 	l10n_parent int(11) DEFAULT '0' NOT NULL,
